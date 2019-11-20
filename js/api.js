f518fa8a389d5a59ab5d22ae24aad50a6062329c
abrahem <abrheem16@gmail.com>
2019-11-20 11:44:17 +0300

    Update

--- js/api.js
+++ js/api.js
@@ -1,12 +1,22 @@
-var data = JSON.parse('{"featured":[{"id":"17413","title":"Late Night","large":"s3\/images\/5dc904bef2ade.jpg","type":"movie","s3":"1"},{"id":"17412","title":"Good Boys","large":"s3\/images\/5dc3ddda1d2e0.jpg","type":"movie","s3":"1"},{"id":"17331","title":"Kill Chain","large":"s3\/images\/5db44d7724d3b.jpg","type":"movie","s3":"1"},{"id":"17131","title":"Bloodline","large":"share\/cdn31\/upload\/2019\/09\/21\/5d862ecec9e31.jpg","type":"movie","s3":"0"},{"id":"17060","title":"Drunk Parents","large":"share\/cdn31\/upload\/2019\/09\/15\/5d7e8faf0a70f.jpg","type":"movie","s3":"0"}]}');
-document.getElementById("sld1").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('http://cinema.nx-iq.com/"+data["featured"][0].large+"')");
-document.getElementById("sld2").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('http://cinema.nx-iq.com/"+data["featured"][1].large+"')");
-document.getElementById("sld3").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('http://cinema.nx-iq.com/"+data["featured"][2].large+"')");
-document.getElementById("sld4").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('http://cinema.nx-iq.com/"+data["featured"][3].large+"')");
-document.getElementById("sld5").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('http://cinema.nx-iq.com/"+data["featured"][4].large+"')");
-
-document.getElementById("txt1").innerText = data["featured"][0].title;
-document.getElementById("txt2").innerText = data["featured"][1].title;
-document.getElementById("txt3").innerText = data["featured"][2].title;
-document.getElementById("txt4").innerText = data["featured"][3].title;
-document.getElementById("txt5").innerText = data["featured"][4].title;
+app.request.json('http://snoanime.com/manga/main.phpp', function (data) {
+document.getElementById("sld1").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('https://media.gmanga.me/uploads/manga/cover/"+data[2]["read"][0]["manga"].id+data[2]["read"][0]["manga"].cover+"')");
+document.getElementById("sld2").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('https://media.gmanga.me/uploads/manga/cover/"+data[2]["read"][1]["manga"].id+data[2]["read"][1]["manga"].cover+"')");
+document.getElementById("sld3").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('https://media.gmanga.me/uploads/manga/cover/"+data[2]["read"][2]["manga"].id+data[2]["read"][2]["manga"].cover+"')");
+document.getElementById("sld4").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('https://media.gmanga.me/uploads/manga/cover/"+data[2]["read"][3]["manga"].id+data[2]["read"][3]["manga"].cover+"')");
+document.getElementById("sld5").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('https://media.gmanga.me/uploads/manga/cover/"+data[2]["read"][4]["manga"].id+data[2]["read"][4]["manga"].cover+"')");
+document.getElementById("sld6").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('https://media.gmanga.me/uploads/manga/cover/"+data[2]["read"][5]["manga"].id+data[2]["read"][5]["manga"].cover+"')");
+document.getElementById("sld7").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('https://media.gmanga.me/uploads/manga/cover/"+data[2]["read"][6]["manga"].id+data[2]["read"][6]["manga"].cover+"')");
+document.getElementById("sld8").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('https://media.gmanga.me/uploads/manga/cover/"+data[2]["read"][7]["manga"].id+data[2]["read"][7]["manga"].cover+"')");
+document.getElementById("sld9").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('https://media.gmanga.me/uploads/manga/cover/"+data[2]["read"][8]["manga"].id+data[2]["read"][8]["manga"].cover+"')");
+document.getElementById("sld10").setAttribute("style","background-size: 100%; background-position: center; border: 100px; background-image: url('https://media.gmanga.me/uploads/manga/cover/"+data[2]["read"][9]["manga"].id+data[2]["read"][9]["manga"].cover+"')");
+document.getElementById("txt1").innerText = data[2]["read"][0]["manga"].title;
+document.getElementById("txt2").innerText = data[2]["read"][1]["manga"].title;
+document.getElementById("txt3").innerText = data[2]["read"][2]["manga"].title;
+document.getElementById("txt4").innerText = data[2]["read"][3]["manga"].title;
+document.getElementById("txt5").innerText = data[2]["read"][4]["manga"].title;
+document.getElementById("txt6").innerText = data[2]["read"][5]["manga"].title;
+document.getElementById("txt7").innerText = data[2]["read"][6]["manga"].title;
+document.getElementById("txt8").innerText = data[2]["read"][7]["manga"].title;
+document.getElementById("txt9").innerText = data[2]["read"][8]["manga"].title;
+document.getElementById("txt10").innerText = data[2]["read"][9]["manga"].title;
+});
