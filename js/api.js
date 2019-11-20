9cb076a585a0721ad2f01583865a15aa09293d82
abrahem <abrheem16@gmail.com>
2019-11-20 15:02:27 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -51,6 +51,12 @@ for (i = 0; i < data[0]["1"].length; i++) {
     to.style = "border-radius: 5px;";
     tr.className = "card-content";
     one.className = "col-50 medium-25";
+    var n = navigator.userAgent.includes("Windows");
+    if (n == true) {
+        one.style = "--f7-cols-per-row: 9;";
+    } else {
+
+    }
     img.src = "https://media.gmanga.me/uploads/manga/cover/"+data[0]["1"][i]["manga"].id+"/large_"+data[0]["1"][i]["manga"].cover+"";
     img.style = "height: 150px; border-radius: 5px;";
     img.setAttribute("width","100%");
@@ -92,6 +98,12 @@ for (ii = 0; ii < data[1]["2"].length; ii++) {
     to.style = "border-radius: 5px;";
     tr.className = "card-content";
     one.className = "col-50 medium-25";
+    var n = navigator.userAgent.includes("Windows");
+    if (n == true) {
+        one.style = "--f7-cols-per-row: 9;";
+    } else {
+
+    }
     img.src = "https://media.gmanga.me/uploads/manga/cover/"+data[1]["2"][ii]["manga"].id+"/large_"+data[1]["2"][ii]["manga"].cover+"";
     img.style = "height: 150px; border-radius: 5px;";
     img.setAttribute("width","100%");
